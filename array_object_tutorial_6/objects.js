58804475ea71bc23a5ac8843e7520560ca190a20
unknown <zhulei91225@hotmail.com>
2018-04-04 19:08:54 -0700

    add

---
+++ array_object_tutorial_6/objects.js
@@ -0,0 +1,42 @@
+var red = 0,
+	green = 0,
+	blue = 0;
+	
+red = 5;
+green = 10;
+blue = 15;
+
+var colors = {
+	red:0,
+	green:0,
+	blue:0;
+};
+colors.red =5,
+colors.green =10,
+colors.blue =15;
+
+//ARRAYs
+var red =[],
+	green=[],
+	blue=[];
+	
+red.push(5);
+green.push(10);
+blue.push(15),
+names=[],
+tags=[],
+searchkeys=[];
+
+//objects
+var colors =[]
+colors.push({
+	red:5,
+	green:10,
+	blue:15,
+	name:"whatever",
+	tags:"whatever",
+	searchkeys:"etc"
+	});
+
+el.style.backgroundColor="rgb("+red[0]+","+green[0]+","+blue[0]+")";
+el.style.backgroundColor="rgb("+colors[0]+","+colors[0]+","+colors[0]+")";
